1c91f3ed7174e174bbb3d9f7bf64f9a464f7cbe1
Carlos Faustino <chfcchfc96@gmail.com>
2024-07-12 16:08:36 -0300

    feat: add dayjs instance

---
+++ backend/src/lib/dayjs.ts
@@ -0,0 +1,9 @@
+import "dayjs/locale/pt-br"
+
+import dayjs from "dayjs"
+import localizedFormat from "dayjs/plugin/localizedFormat"
+
+dayjs.extend(localizedFormat)
+dayjs.locale("pt-br")
+
+export { dayjs }
